c5021d614076a9404ccdefb2cd1f7cf453535f74
jakub-wojciechowski <jakub@alice.si>
2018-06-04 23:28:52 +0200

    Access granting UI

--- app/app.js
+++ app/app.js
@@ -51,7 +51,7 @@ async function deployUsersDirectory() {
 }
 
 async function deployDataAccess() {
-	dataAccess = await UsersDirectory.new({from: mainAccount, gas: 2000000});
+	dataAccess = await DataAccess.new({from: mainAccount, gas: 2000000});
 	console.log("Deployed dataAccess: " + dataAccess.address);
 	localStorage.setItem('dataAccessAddress', dataAccess.address);
 }
@@ -70,6 +70,11 @@ async function addUserElement(parentId, address) {
 	addDirectoryElement(parentId, address);
 	listenToEvents();
 };
+
+async function grantAccess(folder, group, read, write, admin) {
+	await dataAccess.changeAccess(folder, group, read, write, admin, {from: mainAccount, gas: 2000000});
+	M.toast({html: "Access granted for: " + folder + " to: " + group + " [ read: " + read + " write: " + write + " admin: " + admin + " ]"})
+}
 
 
 async function fetchDirectory(contract, displayFunc, elementId, parentId) {
@@ -117,6 +122,25 @@ async function getDataDirectory() {
 }
 
 
+async function getDataAccess() {
+	DataAccess.setProvider(web3.currentProvider);
+	var address = localStorage.getItem('dataAccessAddress');
+	if (address) {
+		dataAccess = await DataAccess.at(address);
+	} else {
+		await deployDataDirectory();
+	}
+}
+
+async function getContracts() {
+	await getDataAccess();
+	await getUsersDirectory();
+	await getDataDirectory();
+
+	listenToEvents();
+}
+
+
 async function getUsersDirectory() {
 	UsersDirectory.setProvider(web3.currentProvider);
 	var address = localStorage.getItem('usersDirectoryAddress');
@@ -127,12 +151,9 @@ async function getUsersDirectory() {
 	}
 	await fetchUsersDirectory("root-users");
 
-	listenToEvents();
 }
 
 function listenToEvents() {
-	//Listen to events
-	window.dd = dataDirectory;
 
 	dataDirectory.AddedElement({}, {fromBlock:1, toBlock:'latest'}).get(function(error, results) {
 		results.forEach(function(result) {
@@ -146,13 +167,27 @@ function listenToEvents() {
 	});
 
 	usersDirectory.AddedUser({}, {fromBlock:1, toBlock:'latest'}).get(function(error, results) {
-		console.log("Checking added user");
-		console.log(results);
 		results.forEach(function(result) {
 			var event = {
 				block: result.blockNumber,
 				tx: result.transactionHash,
 				desc: "A user " + result.args.user + " has been added to the folder " + result.args.parentId + " by the admin " + result.args.admin + "]"
+			};
+			displayEvent(event);
+		});
+	});
+
+	dataAccess.AccessChanged({}, {fromBlock:1, toBlock:'latest'}).get(function(error, results) {
+		console.log(results);
+		results.forEach(function(result) {
+			var event = {
+				block: result.blockNumber,
+				tx: result.transactionHash,
+				desc: "A group " + result.args.group
+				+ " has been given <br/> [ read: " + result.args.read
+				+ ", write: " + result.args.write
+				+ ", admin: " + result.args.admin
+				+ " ] access to the folder: " + result.args.directory
 			};
 			displayEvent(event);
 		});
@@ -180,8 +215,7 @@ window.onload = function() {
       mainAccount = accs[0];
 		  console.log("Main account: " + mainAccount);
 
-			getDataDirectory();
-			getUsersDirectory();
+			getContracts();
 
 	});
 
@@ -243,6 +277,8 @@ window.addUserDirectoryFolder = function(parentId, title, id) {
 		'Donors' : 'face'
 	};
 
+	$("#selectedGroup").append('<option value="' + id + '">' + title + '</option>');
+
 	var parent = $('#' + parentId);
 	var elem = $('<li><div class="collapsible-header"><i class="material-icons">' + (defaultIcons[title] || 'folder_item') + '</i>'
 		+ title + '</div><div class="collapsible-body"><div class="row"><div class="col s12 m12">'
@@ -296,6 +332,20 @@ window.addUser = function(parentId) {
 window.selectDataFolder = function(id, title) {
 	console.log("Select: " + id + title);
 	$("#currentFolder").val(title);
+};
+
+window.grantAccess = function() {
+  var folder = $("#currentFolder").val();
+  var group = $('#selectedGroup').find(":selected").val();
+  var read = $('#readAccess:checked').val() == 'on';
+  var write = $('#writeAccess:checked').val() == 'on';
+  var admin = $('#adminAccess:checked').val() == 'on';
+	grantAccess(folder, group, read, write, admin);
+};
+
+window.test = function() {
+	dataAccess.changeAccess("directory", "group", false, false, false, {from: mainAccount, gas: 2000000});
+	console.log(dataAccess);
 };
 
 
